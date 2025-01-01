595b99a6a2f55b2c44243a53a5010e926d6dcb57
Cody Epstein <codyepstein@gmail.com>
2024-12-31 18:55:38 -0800

    connect to mongo database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
+const mongo = new MongoClient(process.env.DB_URL);
+await mongo.connect();
+const databasesList = await mongo.db().admin().listDatabases();
+
+console.log("Databases:", databasesList);
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
