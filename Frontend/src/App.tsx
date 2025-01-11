ca118cf8d4be8f865b1ac88e9397212086f1c1b9
Cody Epstein <codyepstein@gmail.com>
2025-01-11 15:34:59 -0800

    added clerk

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router";
+import Header from "./components/Header/Header";
+
 function App() {
 	return (
 		<>
+			<Header />
+			<Outlet />
 			<h1 className="text-red-400">Hello World</h1>
 		</>
 	);
