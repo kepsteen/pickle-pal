786ace1218a47b64365dbd2e0b06c2460e922e2e
Cody Epstein <codyepstein@gmail.com>
2025-01-12 10:10:01 -0800

    fix Outlet

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router";
 
-import GlobalHeader from "./components/Header/GlobalHeader/GlobalHeader";
-
 function App() {
 	return (
 		<>
-			<GlobalHeader />
 			<Outlet />
-			<h1 className="text-red-400">Hello World</h1>
 		</>
 	);
 }
