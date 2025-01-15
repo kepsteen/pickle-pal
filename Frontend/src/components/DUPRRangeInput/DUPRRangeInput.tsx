d5acfd6c70b4bbf9ab7a0159bb8dd6d23e2293be
Cody Epstein <codyepstein@gmail.com>
2025-01-15 10:23:58 -0800

    fix DUPRRangeInput component

--- Frontend/src/components/DUPRRangeInput/DUPRRangeInput.tsx
+++ Frontend/src/components/DUPRRangeInput/DUPRRangeInput.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 
-export default function DUPRRangeInput() {
+export const DUPRRangeInput = forwardRef<HTMLInputElement>((props, ref) => {
 	const [value, setValue] = useState(2.0);
 	return (
 		<div className="w-full">
 			<span className="pl-2 text-primary">{value}</span>
 			<input
+				{...props}
 				type="range"
+				ref={ref}
 				min={2}
 				max={8}
 				step={0.1}
@@ -18,4 +20,4 @@ export default function DUPRRangeInput() {
 			/>
 		</div>
 	);
-}
+});
