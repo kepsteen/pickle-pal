25f82f7353d34299bfbb5acceb392e53bdabcd3b
Cody Epstein <codyepstein@gmail.com>
2025-01-16 09:55:25 -0800

    update shell script

--- scripts/ec2env.sh
+++ scripts/ec2env.sh
@@ -4,7 +4,7 @@
 PARAMS=$(aws ssm get-parameters-by-path --path /pickle-pal --query "Parameters[*].{Name:Name,Value:Value}")
 
 # Create Backend .env
-echo $PARAMS | jq -jr 'map(select(.Name | startswith("/pickle-pal/Backend/"))) | .[] | .Name | sub("/pickle-pal/Backend/"; ""), "=", .Value, "\n"' > Backend/.env
+echo $PARAMS | jq -r 'map(select(.Name | startswith("/pickle-pal/Backend/"))) | .[] | "\(.Name | sub("/pickle-pal/Backend/"; ""))=\(.Value)"' > Backend/.env
 
 # Create Frontend .env
 echo $PARAMS | jq -jr 'map(select(.Name | startswith("/pickle-pal/Frontend/"))) | .[] | .Name | sub("/pickle-pal/Frontend/"; "VITE_"), "=", .Value, "\n"' > Frontend/.env
