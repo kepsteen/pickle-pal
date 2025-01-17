5eeecafa2186283334748e07682f4e5fe83bef9c
Cody Epstein <codyepstein@gmail.com>
2025-01-17 10:17:21 -0800

    add change to ensure redploy worked properly

--- Frontend/src/pages/LandingPage.tsx
+++ Frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,10 @@
 import ClerkHeader from "../components/Headers/ClerkHeader";
 
 export default function LandingPage() {
-	return <ClerkHeader />;
+	return (
+		<>
+			<ClerkHeader />
+			<h1>Redeploy worked</h1>
+		</>
+	);
 }
