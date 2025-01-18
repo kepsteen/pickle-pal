826e9189d34a9ab79eacf0a173a258aab5f6520d
preethim111 <preethimanne11@gmail.com>
2025-01-18 14:05:31 -0800

    created user schema

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -4,6 +4,30 @@ const userSchema = mongoose.Schema(
 	{
 		email: { type: String, required: true },
 		firstName: { type: String, required: true },
+		skillLevel: {  type: String, enum: ['Beginner', 'Intermediate', 'Advanced'] ,required: true},
+		playStyle: { type: String, enum: ['Dinker', 'Hybrid', 'Banger'], required: true },
+		lookingFor: {
+			casual: {
+				type: Boolean,
+				default: false
+			},
+			competitive: {
+				type: Boolean,
+				default: false
+			},
+			friends: {
+				type: Boolean,
+				default: false
+			},
+			drilling: {
+				type: Boolean,
+				default: false
+			},
+			
+		},
+		bio: { type: String, required: false },
+		profileImage: { type: String, required: false},
+		duprRating: { type: Number, min: 2, max: 8, required: false }
 	},
 	{
 		collection: "users",
