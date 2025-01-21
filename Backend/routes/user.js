f22d6e3159e118304e6189760dee2cf73e737f8e
Cody Epstein <codyepstein@gmail.com>
2025-01-20 22:41:14 -0800

    configure multer and add middleware to route

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,14 +1,32 @@
 import express from "express";
 import {
-	createUser,
+	createInitialProfile,
 	verifyEmailExists,
 	updateProfile,
 } from "../controllers/user.controllers.js";
+import multer from "multer";
+
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({
+	storage: storage,
+	limits: {
+		fileSize: 10 * 1024 * 1024,
+	},
+
+	fileFilter: (req, file, cb) => {
+		if (file.mimetype.startsWith("image/")) {
+			cb(null, true);
+		} else {
+			cb(new Error("Only image files are allowed!"), false);
+		}
+	},
+});
+
 // Create Profile
-router.post("/", createUser);
+router.post("/", createInitialProfile);
 router.get("/:email", verifyEmailExists);
-router.patch("/:clerkId", updateProfile);
+router.patch("/:userId", upload.single("profileImage"), updateProfile);
 
 export const usersRouter = router;
