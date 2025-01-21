1aaadac80a74808fd17ac7d20112236db5ef57f4
Cody Epstein <codyepstein@gmail.com>
2025-01-20 22:39:57 -0800

    add updateProfile controller and rename some things

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -1,10 +1,19 @@
-import User from "../models/user.model.js";
+import { formatUserImageName } from "../lib/utils.js";
+import { InitialProfile } from "../models/profile.model.js";
+import { uploadToS3 } from "../s3/client.js";
+import dotenv from "dotenv";
 
-export const createUser = async (req, res) => {
+if (process.env.NODE_ENV === "production") {
+	dotenv.config({ path: "/etc/app.env" });
+} else {
+	dotenv.config({ path: "../.env" });
+}
+
+export const createInitialProfile = async (req, res) => {
 	try {
 		const { email, firstName } = req.body;
 		// Todo: Add error handling
-		const newProfile = await User.create({ email, firstName });
+		const newProfile = await InitialProfile.create({ email, firstName });
 		res.status(201).json(newProfile);
 	} catch (error) {
 		res.status(500).json({ error: "Failed to create profile" });
@@ -15,7 +24,7 @@ export const verifyEmailExists = async (req, res) => {
 	try {
 		const { email } = req.params;
 		if (email === undefined) throw new Error("Email is required");
-		const profile = await User.find({ email });
+		const profile = await InitialProfile.find({ email });
 		if (profile.length !== 0) {
 			return res.status(200).json({ uniqueEmail: false });
 		}
@@ -28,8 +37,46 @@ export const verifyEmailExists = async (req, res) => {
 
 export const updateProfile = async (req, res) => {
 	try {
-		const { clerkId } = req.params;
+		const { userId } = req.params;
+		const profileImage = req.file;
+		const { firstName, skillLevel, playStyle, duprRating, bio, lookingFor } =
+			req.body;
 
-		console.log("req.body", req.body);
-	} catch (error) {}
+		if (!profileImage) {
+			return res.status(400).json({ error: "Profile image is required" });
+		}
+
+		const imageName = formatUserImageName(
+			profileImage.originalname,
+			userId,
+			"profileImgs"
+		);
+
+		await uploadToS3(profileImage.buffer, imageName, userId);
+
+		let updateData = {
+			firstName,
+			skillLevel,
+			playStyle,
+			duprRating,
+			bio,
+			lookingFor,
+			profileImageUrl: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${imageName}`,
+		};
+
+		const updatedProfile = await InitialProfile.findOneAndUpdate(
+			{ userId },
+			updateData,
+			{ new: true }
+		);
+
+		if (!updatedProfile) {
+			return res.status(404).json({ error: "Profile not found" });
+		}
+
+		return res.status(200).json(updatedProfile);
+	} catch (error) {
+		console.error("Profile update error:", error);
+		res.status(500).json({ error: "Error updating Profile" });
+	}
 };
