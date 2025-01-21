323189825098e2da04d25eaf4d0a0ab6879917f6
Cody Epstein <codyepstein@gmail.com>
2025-01-20 23:15:26 -0800

    remove unused getValues method

--- Frontend/src/pages/Onboarding/OnboardingPage.tsx
+++ Frontend/src/pages/Onboarding/OnboardingPage.tsx
@@ -28,7 +28,6 @@ export default function OnboardingPage({ isEditing }: OnboardingPageProps) {
 	const {
 		register,
 		handleSubmit,
-		getValues,
 		formState: { errors },
 	} = useForm<ProfileFormData>({
 		resolver: zodResolver(profileSchema),
