0e63edf258fabf3adf35ed54859c7a7327d1181c
Cody Epstein <codyepstein@gmail.com>
2025-01-20 22:41:23 -0800

    remove commented out code

--- Frontend/src/components/ProfileImgInput/ProfileImgInput.tsx
+++ Frontend/src/components/ProfileImgInput/ProfileImgInput.tsx
@@ -1,21 +1,6 @@
 import { ImagePlus } from "lucide-react";
 import { forwardRef } from "react";
 
-// export default function ProfileImgInput() {
-// 	return (
-// 		<div className="relative">
-// 			<input
-// 				name="profileImgInput"
-// 				type="file"
-// 				className="w-full opacity-0 h-72"
-// 			/>
-// 			<div className="absolute inset-0 grid border cursor-pointer place-content-center bg-base-300 rounded-box border-accent">
-// 				<ImagePlus className="w-10 h-10 text-primary" />
-// 			</div>
-// 		</div>
-// 	);
-// }
-
 type ProfileImgInputProps = {
 	name: string;
 	preview?: string;
