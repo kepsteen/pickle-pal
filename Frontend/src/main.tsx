a3080163512d22aee9897d5ee7ad0c2ef49f902c
Cody Epstein <codyepstein@gmail.com>
2025-01-21 15:42:02 -0800

    add react query

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -12,11 +13,15 @@ if (!PUBLISHABLE_KEY) {
 	throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
 			<BrowserRouter>
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
 			</BrowserRouter>
 			<div id="clerk-captcha" />
 		</ClerkProvider>
