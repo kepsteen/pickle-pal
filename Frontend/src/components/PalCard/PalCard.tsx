1fda4b0dc4e60122261757bac2e74a787f16a821
Cody Epstein <codyepstein@gmail.com>
2025-01-22 23:53:09 -0800

    modify palCard

--- Frontend/src/components/PalCard/PalCard.tsx
+++ Frontend/src/components/PalCard/PalCard.tsx
@@ -31,12 +31,12 @@ function renderLookingForBadge(item: string) {
 
 type PalCardProps = {
 	profile: ProfileData;
-	className: string;
+	className?: string;
 };
 
 export default function PalCard({ profile, className }: PalCardProps) {
 	return (
-		<Card className={cn("w-full mx-4 rounded-b-md max-w-96", className)}>
+		<Card className={cn("mx-4 rounded-b-md w-[500px]", className)}>
 			<CardTitle className="bg-base-200 rounded-t-md">
 				<div className="flex items-center gap-2">
 					<h1 className="text-lg font-semibold">{profile.firstName}</h1>
@@ -46,27 +46,25 @@ export default function PalCard({ profile, className }: PalCardProps) {
 				</div>
 			</CardTitle>
 			<CardContent className="flex flex-col bg-base-200 rounded-b-md">
-				<div className="w-full mb-4 h-60">
+				<div className="flex-shrink-0 w-full mb-4 h-96">
 					<img
 						src={profile.profileImageUrl}
 						alt={`${profile.firstName}'s profile picture`}
 						className="object-cover object-top w-full h-full rounded-md"
 					/>
 				</div>
-				<h2 className="font-semibold">Playstyle</h2>
+				<h2 className="flex-shrink-0 font-semibold">Playstyle</h2>
 				<Badge
 					variant="outline"
 					size="md"
-					className="px-4 border-info text-info"
+					className="flex-shrink-0 px-4 border-info text-info"
 				>
 					{profile.playStyle}
 				</Badge>
-				<h2 className="font-semibold">Looking For</h2>
-				<div className="flex flex-wrap gap-2">
+				<h2 className="flex-shrink-0 font-semibold">Looking For</h2>
+				<div className="flex flex-wrap flex-shrink-0 gap-2">
 					{profile.lookingFor.map((item) => renderLookingForBadge(item))}
 				</div>
-				<h2 className="font-semibold">Bio</h2>
-				<p className="font-normal text-muted">{profile.bio}</p>
 			</CardContent>
 		</Card>
 	);
--- Frontend/src/pages/Home/HomePage.tsx
+++ Frontend/src/pages/Home/HomePage.tsx
@@ -3,11 +3,18 @@ import PalCard from "../../components/PalCard/PalCard";
 import SwipeButton from "../../components/SwipeButton/SwipeButton";
 import { ProfileData } from "../../types/profileSchema";
 import { getUsers } from "../../lib/api";
+import { useState } from "react";
 
 export default function HomePage() {
+	const [profiles, setProfiles] = useState<ProfileData[]>([]);
+	const [index, setIndex] = useState(5);
 	const query = useQuery({
 		queryKey: ["profiles"],
-		queryFn: () => getUsers("user_2rgLaUCcJmigG45v2SAn89Bex9O"),
+		queryFn: async () => {
+			const data = await getUsers("user_2rgLaUCcJmigG45v2SAn89Bex9O");
+			setProfiles(data);
+			return data;
+		},
 	});
 
 	if (query.isLoading) return <div>Loading...</div>;
@@ -16,18 +23,22 @@ export default function HomePage() {
 
 	return (
 		<>
-			<div className="grid grid-cols-1 grid-rows-1 place-content-center">
-				{query.data?.map((user: ProfileData) => (
-					<PalCard
-						key={`${user.profileImageUrl}-${user.duprRating}`}
-						profile={user}
-						className="col-start-1 row-start-1"
-					/>
-				))}
+			<div className="grid place-content-center">
+				<PalCard profile={profiles[index]} />
 			</div>
 			<div className="flex justify-center gap-8 mt-8">
-				<SwipeButton variant="dislike" />
-				<SwipeButton variant="like" />
+				<SwipeButton
+					variant="dislike"
+					onClick={() => {
+						setIndex((prev) => prev - 1);
+					}}
+				/>
+				<SwipeButton
+					variant="like"
+					onClick={() => {
+						setIndex((prev) => prev + 1);
+					}}
+				/>
 			</div>
 		</>
 	);
