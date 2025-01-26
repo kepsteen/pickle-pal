af9767418d81b5b079d8d341f4ae548b9f0476b3
Cody Epstein <codyepstein@gmail.com>
2025-01-26 14:37:56 -0800

    add nearby users route

--- Backend/routes/location.js
+++ Backend/routes/location.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { requireAuth } from "@clerk/express";
-import { setLocation } from "../controllers/location.controllers.js";
+import {
+	setLocation,
+	getNearByUsers,
+} from "../controllers/location.controllers.js";
 
 const router = express.Router();
 
 router.post("/", requireAuth(), setLocation);
+router.get("/nearby-users", requireAuth(), getNearByUsers);
 
 export const locationsRouter = router;
