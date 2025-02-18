1548305065540abe6853e2a1134734b9249c687c
Cody Epstein <codyepstein@gmail.com>
2025-02-18 14:50:10 -0800

    fix build error

--- Frontend/src/components/Toast/Toast.tsx
+++ Frontend/src/components/Toast/Toast.tsx
@@ -2,12 +2,13 @@ import { Avatar } from "../Avatar/Avatar";
 
 type MatchToastProps = {
 	name: string;
+	imageUrl: string;
 };
 
-export const MatchToast = ({ name }: MatchToastProps) => {
+export const MatchToast = ({ name, imageUrl }: MatchToastProps) => {
 	return (
 		<div className="relative flex items-center justify-start w-full gap-4">
-			<Avatar />
+			<Avatar imageUrl={imageUrl} />
 			<span className="text-xl font-semibold text-base-content">{name}</span>
 			<span className="absolute text-lg -top-2 -right-1 text-primary">
 				New Pal!
